a70710ee2600a40c156ea95c501b253bfcb6e5a4
Danielgruvinelo <danielrochagruvinelodossantos@gmail.com>
2026-02-09 15:17:50 +0000

    criado comando diga_ola

--- ola.py
+++ ola.py
@@ -1,1 +1,8 @@
-print('Olá Mundo!!!')
+def diga_ola():
+    print('Olá Mundo!!!')
+    nome = input('Digite seu nome: ')
+    print(f'Olá {nome}! Seja bem vindo(a). ')
+    idade = int(input('Por favor me diga sua idade: '))
+    print(f'Perfeito {nome}. Você tem {idade} anos. ')
+    
+diga_ola()
